9a22884a74cc73a4ecb8e0b8a971f6278df7c13b
tomekczerwinski <CludiS6@gmail.com>
2013-03-18 11:34:00 +0100

    Update zad1.c

--- lab05/zad1.c
+++ lab05/zad1.c
@@ -1,1 +1,23 @@
-moje
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void funk(int n, int k, int *q, int *r)
+    {
+        *q = n / k;
+        *r = n % k;
+
+    }
+
+
+int main()
+{
+    int a, q, r;
+    int b;
+    scanf("%i",&a);
+    scanf("%i",&b);
+    funk(a,b, &q, &r);
+
+    printf("Iloraz = %i \nReszta = %i", q, r);
+    return 0;
+}
